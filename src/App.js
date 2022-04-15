d96176eb08c92ed37bad94e3998261bb183e1c8e
Sajalhowlader <sajolhowlader08@gmail.com>
2022-04-15 14:58:40 +0600

    add some components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
 
+
+
+
+      </Router>
     </div>
   );
 }
